250524724c28373123949a74ad7a0b88b4923e7f
WitherZx <83796995+WitherZx@users.noreply.github.com>
2026-02-18 18:15:36 -0300

    Simplify Cloudflare config to empty object

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  wrapper: "cloudflare-node",
-  converter: "aws-apigw-v2",
-  incrementalCache: "dummy",
-  tagCache: "dummy",
-  queue: "direct",
-});
+export default defineCloudflareConfig({});
