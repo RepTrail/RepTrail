30d06955cd10839173bacfe2e098e82a12eabe4d
WitherZx <83796995+WitherZx@users.noreply.github.com>
2026-02-18 16:36:58 -0300

    Fix quotes in open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,9 @@
-import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
-  override: {
-    wrapper: 'cloudflare-node',
-    converter: 'aws-apigw-v2',
-    incrementalCache: 'dummy',
-    tagCache: 'dummy',
-    queue: 'sqs-lite',
-  },
+  wrapper: "cloudflare-node",
+  converter: "aws-apigw-v2",
+  incrementalCache: "dummy",
+  tagCache: "dummy",
+  queue: "sqs-lite",
 });
