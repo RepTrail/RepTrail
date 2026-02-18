a65b56a01ba103b9f568d26ed44be4477bdf3fed
WitherZx <83796995+WitherZx@users.noreply.github.com>
2026-02-18 16:29:29 -0300

    Refactor default configuration in open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,11 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 
 export default defineCloudflareConfig({
-  default: {
-    override: {
-      wrapper: 'cloudflare-node',
-      converter: 'aws-apigw-v2',
-      incrementalCache: 'dummy',
-      tagCache: 'dummy',
-      queue: 'sqs-lite',
-    },
+  override: {
+    wrapper: 'cloudflare-node',
+    converter: 'aws-apigw-v2',
+    incrementalCache: 'dummy',
+    tagCache: 'dummy',
+    queue: 'sqs-lite',
   },
 });
