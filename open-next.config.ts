f1ecf65487fb328540186536eb32619c9b72c77c
WitherZx <marcos.contatoprof@gmail.com>
2026-02-20 03:55:27 -0300

    build: enable incremental splitting without suppress directive

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-// @ts-expect-error - incremental is needed for bundle splitting on free tier
 export default defineCloudflareConfig({
     incremental: true,
     minify: true,
