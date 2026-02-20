2213cc360d8df3edb833708d50117e0209c73202
WitherZx <marcos.contatoprof@gmail.com>
2026-02-20 03:51:49 -0300

    build: attempt incremental splitting in open-next.config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({});
+// @ts-expect-error - incremental is needed for bundle splitting on free tier
+export default defineCloudflareConfig({
+    incremental: true,
+    minify: true,
+});
